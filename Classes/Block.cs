9cd7f68c711f2ad3cf3e112f994c88eb785194ed
Duviz 2 <javierduviz2@gmail.com>
2026-03-09 19:55:21 +0100

    asd

--- Classes/Block.cs
+++ Classes/Block.cs
@@ -16,13 +16,15 @@ public class Block : ScriptableObject
     public int chance = 0;
     [Space]
     public Texture texture;
+    public Material mat;
     public BlockType type = BlockType.block;
 
-    public Block(string iD, Texture texture, BlockType type, int chance)
+    public Block(string iD, Texture texture, BlockType type, int chance, Material mat = null)
     {
         ID = iD;
         this.texture = texture;
         this.type = type;
         this.chance = chance;
+        if (mat) this.mat = mat;
     }
 }
